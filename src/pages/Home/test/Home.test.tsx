7e31909f26fd40e3110452a59fe27ea1c00e6b44
Николай <Doonn3@gmail.com>
2023-03-19 18:55:24 +0300

    feat: add home test

---
+++ src/pages/Home/test/Home.test.tsx
@@ -0,0 +1,13 @@
+// import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+// import Card from '../../../components/Card/Card';
+// import FakeCards from '../../../FakeApi/cards.json';
+// import Home from '../Home';
+
+describe('Home', () => {
+  test('Проверка на количество, Карточек', () => {
+    // const keys = Object.keys(FakeCards);
+    // const { container } = render(<Home />);
+    // const cards = container.querySelectorAll('.card');
+  });
+});
