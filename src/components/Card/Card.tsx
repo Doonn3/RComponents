359a6dc0df8a0b4973e79a70be3f9f65cd561a01
Николай <Doonn3@gmail.com>
2023-03-26 21:02:28 +0300

    feat: add card theme

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,13 +5,32 @@ import { ReactComponent as IconLike } from '../../assets/like.svg';
 import { ReactComponent as IconView } from '../../assets/view.svg';
 import ImageBlank from '../../assets/blank-card.png';
 
-class Card extends React.Component<CardType> {
+type darkThemeType = {
+  dark: string;
+  whiteText: string;
+};
+
+interface IState {
+  isDarkMode: boolean;
+  darkTheme: darkThemeType;
+}
+
+class Card extends React.Component<CardType, IState> {
+  private readonly theme = { dark: 'dark', whiteText: 'white' };
+
   constructor(props: CardType) {
     super(props);
+    this.state = { isDarkMode: false, darkTheme: { dark: '', whiteText: '' } };
   }
 
   public componentDidMount(): void {
-    //
+    const { darkMode } = this.props;
+    if (darkMode) {
+      this.setState({ isDarkMode: true });
+      // if (this.state.isDarkMode) {
+      this.setState({ darkTheme: this.theme });
+      // }
+    }
   }
 
   public componentWillUnmount(): void {
@@ -24,23 +43,24 @@ class Card extends React.Component<CardType> {
 
   public render(): React.ReactNode {
     const { img, author, title, tags, liksCount, viewCount } = this.props;
+
     return (
-      <div className="card">
+      <div className={`card ${this.state.darkTheme.dark}`}>
         <img className="card__img" src={img || ImageBlank} alt="" />
         <div className="card__content">
-          <p className="card__title">{title}</p>
-          <p className="card__author">by {author}</p>
-          <p className="card__tags">{tags}</p>
+          <p className={`card__title`}>{title}</p>
+          <p className={`card__author ${this.state.darkTheme.whiteText}`}>by {author}</p>
+          <p className={`card__tags ${this.state.darkTheme.whiteText}`}>{tags}</p>
           <div className="card__social">
             <div className="card__likes">
-              <IconLike className="icon icon-like"></IconLike>
-              <span className="likes">
+              <IconLike className={`icon icon-like ${this.state.darkTheme.whiteText}`}></IconLike>
+              <span className={`likes ${this.state.darkTheme.whiteText}`}>
                 {liksCount !== undefined && liksCount <= -1 ? 0 : liksCount || 0}
               </span>
             </div>
             <div className="card__views">
-              <IconView className="icon icon-view"></IconView>
-              <span className="views">
+              <IconView className={`icon icon-view ${this.state.darkTheme.whiteText}`}></IconView>
+              <span className={`views ${this.state.darkTheme.whiteText}`}>
                 {viewCount !== undefined && viewCount <= -1 ? 0 : viewCount || 0}
               </span>
             </div>
