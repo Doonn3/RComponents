8d84c474bb8f5ff0ebf40a5a72698d1c30cbd66b
Николай <Doonn3@gmail.com>
2023-03-26 21:10:18 +0300

    feat: add input radio switch and style

---
+++ src/components/CForm/components/InputSwitch/CInputSwitch.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './style.css';
+
+class CInputSwitch extends React.Component {
+  private inputRef: React.RefObject<HTMLInputElement> = React.createRef();
+
+  public get InputRef() {
+    return this.inputRef;
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div className="container">
+        <div className="selector">
+          <div className="selecotr-item">
+            <input
+              id="radio1"
+              className="selector-item_radio"
+              type="radio"
+              name="selector"
+              defaultChecked
+            />
+            <label className="selector-item_label" htmlFor="radio1">
+              radio 1
+            </label>
+          </div>
+          <div className="selecotr-item">
+            <input id="radio2" className="selector-item_radio" type="radio" name="selector" />
+            <label className="selector-item_label" htmlFor="radio2">
+              radio 2
+            </label>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CInputSwitch;
