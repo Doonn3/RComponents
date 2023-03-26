26dddc52b4c8cfb56f1e410b566b357334cbc354
Николай <Doonn3@gmail.com>
2023-03-26 21:10:48 +0300

    feat: add test for input radio switch

---
+++ src/components/CForm/components/InputSwitch/CinputSwitch.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import CInputSwitch from './CInputSwitch';
+
+describe('CInputSwitch test', () => {
+  test('render component', () => {
+    const { container } = render(<CInputSwitch />);
+    const inputElements = container.querySelectorAll('input');
+    expect(inputElements.length === 2).toBeTruthy();
+  });
+});
