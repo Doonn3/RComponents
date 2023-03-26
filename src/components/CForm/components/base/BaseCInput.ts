31fef65b199dbc41fc7551f318d1dcc731be2724
Николай <Doonn3@gmail.com>
2023-03-26 21:03:59 +0300

    feat: add base component

---
+++ src/components/CForm/components/base/BaseCInput.ts
@@ -0,0 +1,9 @@
+import IAccessCInput from 'components/CForm/interface/IAccessCInput';
+import CInputAccessType from 'components/CForm/types/CInputAccessType';
+import React from 'react';
+
+class BaseCInput {
+  //
+}
+
+export default BaseCInput;
