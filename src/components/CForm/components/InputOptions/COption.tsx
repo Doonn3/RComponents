84b9496054f987bf9c4c854382053b46369c13f7
Николай <Doonn3@gmail.com>
2023-03-26 21:09:35 +0300

    feat: add options and style

---
+++ src/components/CForm/components/InputOptions/COption.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import style from './style.module.css';
+
+interface IState {
+  value: string;
+}
+
+class CInputOption extends React.Component<object, IState> {
+  private inputRef: React.RefObject<HTMLInputElement> = React.createRef();
+
+  public get InputRef() {
+    return this.inputRef;
+  }
+
+  constructor(props = {}) {
+    super(props);
+    this.state = { value: 'lime' };
+  }
+
+  public render(): React.ReactNode {
+    return (
+      <select className={style.options} value={this.state.value} onChange={this.handleChange}>
+        <option value="photo">Photo</option>
+        <option value="nature">Nature</option>
+        <option value="design">Design</option>
+        <option value="illustration">Illustration</option>
+      </select>
+    );
+  }
+
+  private handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({ value: event.target.value });
+  };
+}
+
+export default CInputOption;
