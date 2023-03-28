d3a89496b0b25dfa5e7f9d94f5caa9f309277a3f
Николай <Doonn3@gmail.com>
2023-03-28 08:54:54 +0300

    refactor: delete

--- src/components/CForm/components/base/BaseCInput.ts
+++
@@ -1,9 +0,0 @@
-// import IAccessCInput from 'components/CForm/interface/IAccessCInput';
-// import CInputAccessType from 'components/CForm/types/CInputAccessType';
-// import React from 'react';
-
-class BaseCInput {
-  //
-}
-
-export default BaseCInput;
