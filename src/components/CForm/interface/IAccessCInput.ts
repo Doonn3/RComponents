4918f76316b977f95a488aa071faff6122400fd7
Николай <Doonn3@gmail.com>
2023-04-02 16:00:12 +0300

    refactor: delete

--- src/components/CForm/interface/IAccessCInput.ts
+++
@@ -1,7 +0,0 @@
-interface IAccessCInput {
-  accessTextError(errorText?: string): void;
-  accessTextSuccess(successText?: string): void;
-  resetAccessText(): void;
-}
-
-export default IAccessCInput;
