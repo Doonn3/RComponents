b1315cdfe42c8ecd6aa8e60a861261ddc1a6b38f
Николай <Doonn3@gmail.com>
2023-04-02 16:01:57 +0300

    refactor: search bar class to function

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,50 +1,47 @@
-import React from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { ReactComponent as SearchIcon } from '../../assets/search_icon.svg';
 import './style.css';
 
-class SearchBar extends React.Component<object, { value: string }> {
-  private localKey = 'search-bar';
+function SearchBar() {
+  const localKey = 'search-bar';
+  const [value, setValue] = useState<string>('');
 
-  public constructor(props = {}) {
-    super(props);
-    this.state = { value: '' };
-  }
-
-  public componentDidMount() {
-    const val = localStorage.getItem(this.localKey);
-
+  const mount = () => {
+    const val = localStorage.getItem(localKey);
     if (val !== null) {
-      this.setState({ value: val });
+      setValue(val);
     }
-  }
-
-  public componentWillUnmount() {
-    localStorage.setItem(this.localKey, this.state.value);
-  }
-
-  public componentDidUpdate() {
-    localStorage.setItem(this.localKey, this.state.value);
-  }
-
-  render(): React.ReactNode {
-    return (
-      <div className="search-bar">
-        <SearchIcon className="search-bar__icon" />
-        <input
-          className="search-bar__input"
-          type="text"
-          placeholder="Search"
-          value={this.state.value}
-          onChange={this.changeText}
-        />
-      </div>
-    );
-  }
-
-  private changeText = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const val: string = event.currentTarget.value;
-    this.setState({ value: val });
   };
+
+  const unmount = useCallback(() => {
+    localStorage.setItem(localKey, value);
+  }, [localKey, value]);
+
+  useEffect(() => {
+    mount();
+  }, [localKey]);
+
+  useEffect(() => {
+    unmount();
+  }, [unmount]);
+
+  const changeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const val: string = event.currentTarget.value;
+    setValue(val);
+  };
+
+  return (
+    <div className="search-bar">
+      <SearchIcon className="search-bar__icon" />
+      <input
+        className="search-bar__input"
+        type="text"
+        placeholder="Search"
+        value={value}
+        onChange={changeText}
+      />
+    </div>
+  );
 }
 
 export default SearchBar;
