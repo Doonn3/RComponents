6720e232e598a7f03134633bdd6ccc8a48d99353
Николай <Doonn3@gmail.com>
2023-04-03 08:20:30 +0300

    refactor: main and app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import About from '../pages/About/About';
 import './App.css';
 import Header from '../components/Header/Header';
@@ -8,15 +8,17 @@ import CreateCard from '../pages/CreateCard/CreateCard';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/form" element={<CreateCard />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/form" element={<CreateCard />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 const root = document.getElementById('root') as HTMLElement;
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
