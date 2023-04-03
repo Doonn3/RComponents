4cf765eb26e5f46d50b2fca9ef4a8ce26180a3b5
Николай <Doonn3@gmail.com>
2023-04-03 08:19:13 +0300

    refactor: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest"/>
 /// <reference types="vite/client" />
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
@@ -17,13 +18,14 @@ export default defineConfig({
     }),
   ],
   test: {
+    // exclude: [...configDefaults.exclude, 'src/components/'],
     globals: true,
     environment: 'jsdom',
     coverage: {
       provider: 'c8',
-      all: true,
-      reporter: "text",
-      lines: 50
+      all: false,
+      reporter: 'text',
+      lines: 50,
     },
     setupFiles: './src/setupTest.ts',
   },
