93941f661f606c46796d7e9d9964448ba805ebc1
Николай <Doonn3@gmail.com>
2023-04-09 19:41:37 +0300

    refactor: test delete check card length

--- src/pages/Home/test/Home.test.tsx
+++ src/pages/Home/test/Home.test.tsx
@@ -1,5 +1,4 @@
 import { render } from '@testing-library/react';
-import FakeCards from '../../../FakeApi/cards.json';
 import Home from '../Home';
 
 describe('Home', () => {
@@ -7,12 +6,5 @@ describe('Home', () => {
     const { container } = render(<Home />);
     const searchBar = container.querySelector('.search-bar');
     expect(searchBar?.className).toEqual('search-bar');
-  });
-
-  test('Проверка на количество, Карточек', () => {
-    const keys = Object.keys(FakeCards);
-    const { container } = render(<Home />);
-    const cards = container.querySelectorAll('.card');
-    expect(cards.length).toEqual(keys.length);
   });
 });
