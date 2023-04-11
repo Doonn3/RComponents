1cc112969891b779344a0374430db5c95ea85fe7
Николай <Doonn3@gmail.com>
2023-04-11 10:47:16 +0300

    refactor: home test delete act

--- src/pages/Home/test/Home.test.tsx
+++ src/pages/Home/test/Home.test.tsx
@@ -4,10 +4,8 @@ import Home from '../Home';
 
 describe('Home', () => {
   test('Проверка на search bar', () => {
-    act(() => {
-      const { container } = render(<Home />);
-      const searchBar = container.querySelector('.search-bar');
-      expect(searchBar?.className).toEqual('search-bar');
-    });
+    const { container } = render(<Home />);
+    const searchBar = container.querySelector('.search-bar');
+    expect(searchBar?.className).toEqual('search-bar');
   });
 });
