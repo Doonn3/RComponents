9d694ecbd047569249962c37837f1fad784fe070
Николай <Doonn3@gmail.com>
2023-04-11 10:42:27 +0300

    refactor: import type

--- src/components/ModalCard/ModalCard.test.tsx
+++ src/components/ModalCard/ModalCard.test.tsx
@@ -1,24 +1,6 @@
 import { render } from '@testing-library/react';
 import ModalCard from './ModalCard';
-
-interface PlanetType {
-  imageUrl: string;
-  name: string;
-  descriptions: string;
-  rotation_period: string;
-  orbital_period: string;
-  diameter: string;
-  climate: string;
-  gravity: string;
-  terrain: string;
-  surface_water: string;
-  population: string;
-  residents: string[];
-  films: string[];
-  created: Date;
-  edited: Date;
-  url?: string;
-}
+import PlanetType from '../../api/types/PlanetType';
 
 const type: PlanetType = {
   imageUrl: '',
@@ -41,9 +23,7 @@ const type: PlanetType = {
 
 describe('ModalCard test', () => {
   test('ModalCard render', () => {
-    const click = () => {
-      //
-    };
+    const click = () => {};
     const { container } = render(<ModalCard {...type} onClick={click} />);
     expect(container).toBeInTheDocument();
   });
