b543edf0d8657f435a7d863b83383c00aabc7b4c
Николай <Doonn3@gmail.com>
2023-04-11 10:41:41 +0300

    feat: add act

--- src/pages/Home/test/Home.test.tsx
+++ src/pages/Home/test/Home.test.tsx
@@ -1,10 +1,13 @@
 import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import Home from '../Home';
 
 describe('Home', () => {
   test('Проверка на search bar', () => {
-    const { container } = render(<Home />);
-    const searchBar = container.querySelector('.search-bar');
-    expect(searchBar?.className).toEqual('search-bar');
+    act(() => {
+      const { container } = render(<Home />);
+      const searchBar = container.querySelector('.search-bar');
+      expect(searchBar?.className).toEqual('search-bar');
+    });
   });
 });
