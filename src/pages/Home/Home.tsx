5c17c460660da15538328db14cbf200ed8eb02cb
Николай <Doonn3@gmail.com>
2023-04-16 19:41:42 +0300

    refactor: home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,79 +5,84 @@ import LoadAnim from '../../shared/ui/load/LoadAnim';
 import PlanetsApi from '../../api/PlanetsApi';
 import PlanetType from 'api/types/PlanetType';
 import LayerCards from '../../layers/LayerCards/LayerCards';
+import {
+  useGetPlanetsForPageMidlware,
+  useSearchPlanetMidlware,
+} from '../../api/services/PlanetApi';
+import { useStore } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function Home() {
-  const [data, setData] = useState<PlanetType[]>([]);
-  const [counts, setCount] = useState<string>('1');
+  const store = useStore();
+  // const [data, setData] = useState<PlanetType[]>([]);
+  // const [counts, setCount] = useState<string>();
+  const searchString = useSelector<string>((state) => state.search);
+  // const [isLoader, setLoader] = useState<boolean>(true);
+  // const [query, setQuery] = useState<string>(localStorage.getItem('search-bar') || '');
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const { data, isLoading, error } = useGetPlanetsForPageMidlware(pageNumber);
 
-  const [isLoader, setLoader] = useState<boolean>(true);
-  const [query, setQuery] = useState<string>(localStorage.getItem('search-bar') || '');
-
-  useEffect(() => {
-    (async () => {
-      setLoader(true);
-      if (query.length > 0) {
-        searchResult(query);
-      } else {
-        const data = await getData();
-        if (data === null) {
-          setLoader(false);
-          return;
-        }
-        setCount(data.counts);
-        setData(data.items);
-      }
-      setLoader(false);
-    })();
-  }, [query]);
+  const [search, setSearch] = useState<string>('');
+  const searchData = useSearchPlanetMidlware(search);
+  // useEffect(() => {
+  //   (async () => {
+  //     // setLoader(true);
+  //     // if (query.length > 0) {
+  //       // searchResult(query);
+  //     // } else {
+  //       const data = await getData();
+  //       if (data === null) {
+  //         // setLoader(false);
+  //         return;
+  //       // }
+  //       // setCount(data.counts);
+  //       // setData(data.items);
+  //     // }
+  //     // setLoader(false);
+  //   })();
+  // }, []);
 
   const searchResult = async (value: string) => {
-    setLoader(true);
-    setQuery(value);
-    if (value.length <= 0) {
-      const data = await getData();
-      if (data === null) {
-        setLoader(false);
-        return;
-      }
-      setData(data.items);
-      setLoader(false);
-      return;
-    }
-    const planets: PlanetType[] | null = await PlanetsApi.instance.getSearchPlanets(value);
-    if (planets === null) return;
-    setData(planets);
-    setLoader(false);
+    setSearch(searchString as string);
+    // setSearch(value);
+    // setLoader(true);
+    // setQuery(value);
+    // if (value.length <= 0) {
+    // const data = await getData();
+    // if (data === null) {
+    // setLoader(false);
+    // return;
+    // }
+    // setData(data.items);
+    // setLoader(false);
+    // return;
+    // }
+    // const planets: PlanetType[] | null = await PlanetsApi.instance.getSearchPlanets(value);
+    // if (planets === null) return;
+    // setData(planets);
+    // setLoader(false);
   };
 
   const handlePage = async (val: number) => {
-    setLoader(true);
-    const data = await getData(String(val));
-    if (data === null) {
-      setLoader(false);
-      return;
-    }
-    setData(data.items);
-    setCount(data.counts);
-    setLoader(false);
+    setPageNumber(val);
   };
 
   return (
     <main className="main">
       <SearchBar searchResult={searchResult} />
-      <LayerCards items={data} pageCount={counts} callback={handlePage} />
-      {isLoader ? <LoadAnim /> : ''}
+      {isLoading ? (
+        <LoadAnim />
+      ) : error ? (
+        'Error'
+      ) : (
+        <LayerCards
+          items={search.length <= 0 ? data?.results : searchData.data}
+          maxPageCount={data?.counts || 1}
+          callback={handlePage}
+        />
+      )}
     </main>
   );
-}
-
-async function getData(val = ''): Promise<{ items: PlanetType[]; counts: string } | null> {
-  const data = await PlanetsApi.instance.getAllPlanets(val);
-  if (data === null) return null;
-  const planets: PlanetType[] = data.items;
-  const counts = data.counts;
-
-  return { items: planets, counts };
 }
 
 export default Home;
