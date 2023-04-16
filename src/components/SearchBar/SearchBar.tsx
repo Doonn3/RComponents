767039d55ea5431eb181513a180f05904d5a459a
Николай <Doonn3@gmail.com>
2023-04-16 21:19:33 +0300

    refactor: delete comments

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,48 +1,29 @@
 import { setSearch } from '../../api/slices/search.slice';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { ReactComponent as SearchIcon } from '../../assets/search_icon.svg';
 import './style.css';
-
-// interface ISearchResult {
-//   searchResult: (value: string) => void;
-// }
 
 type RootState = {
   search: string;
 };
 
 function SearchBar() {
-  // const localKey = 'search-bar';
-  // const [value, setValue] = useState<string>('');
-
   const dispatch = useDispatch();
   const text = useSelector<RootState, string>((state) => state.search);
   const [value, setValue] = useState<string>(text || '');
 
-  // useEffect(() => {
-  // localStorage.setItem(localKey, value || '');
-  // }, [value]);
-
   const changeText = (event: React.ChangeEvent<HTMLInputElement>) => {
     const val: string = event.currentTarget.value;
-    // setValue(val);
     dispatch(setSearch(val));
     setValue(val);
-    // if (!props?.searchResult) return;
-    // if (val.length <= 0) {
-    //   props.searchResult(val);
-    // }
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log(text);
-    // if (value.length <= 0) return;
-    // // if (props?.searchResult) {
-    // props.searchResult(value);
-    // }
+    dispatch(setSearch(value));
   };
 
   return (
