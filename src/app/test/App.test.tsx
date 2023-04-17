36a6b3c2e1388bb770cea4479505a93e5275c50e
Николай <Doonn3@gmail.com>
2023-04-18 00:44:51 +0300

    refactpr: test app

--- src/app/test/App.test.tsx
+++ src/app/test/App.test.tsx
@@ -1,9 +1,16 @@
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../../store/Store';
+
 import App from '../App';
 
 describe('App test', () => {
   test('render test', () => {
-    const { container } = render(<App />);
+    const { container } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
     expect(container).toBeTruthy();
 
     const div = container.querySelector('.App');
