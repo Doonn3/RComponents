48cab8325411c58f6232b6873882c38c768ee628
Николай <Doonn3@gmail.com>
2023-04-30 21:21:10 +0300

    refactor: clean comments

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,9 @@
 import express from 'express';
-import fs from 'node:fs/promises';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 import { renderToPipeableStream } from 'react-dom/server';
 import { createServer as createViteServer } from 'vite';
 
 import preFetchPlanets from './preFetchPlanets';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 declare global {
   interface Window {
@@ -32,27 +27,12 @@ async function main() {
 
   app.get('*', async (req, res) => {
     const url = req.originalUrl;
-    let template = await fs.readFile(path.resolve(__dirname, '../index.html'), 'utf-8');
-    const style = await fs.readFile(path.resolve(__dirname, 'styleSSR.css'), 'utf-8');
-    template = await vite.transformIndexHtml(url, template);
     const { renderSSR } = await vite.ssrLoadModule('./src/entry-server.tsx');
 
     const preFetch = await preFetchPlanets();
 
     const appHtml = await renderSSR(url, preFetch);
-    // appHtml = ReactDOMServer.renderToString(appHtml);
 
-    // const templateBuild = template
-    //   .replace(`<div id="root"></div>`, `<div id="root">${appHtml}</div>`)
-    //   .replace('<!-- style -->', `<style>${style}"</style>`)
-    //   .replace(
-    //     '<!--preload-->',
-    //     `<script>window.__PRELOADED_STATE__ = ${JSON.stringify(preFetch).replace(
-    //       /</g,
-    //       '\\u003c'
-    //     )}</script>`
-    //   );
-    // let templateBuild = template;
     const { pipe } = renderToPipeableStream(appHtml, {
       bootstrapModules: ['./src/entry-client.tsx'],
       onShellReady() {
@@ -71,32 +51,12 @@ async function main() {
       onShellError(error) {
         res.statusCode = 500;
         res.setHeader('content-type', 'text/html');
-        res.send('<h1>Something went wrong</h1>');
+        res.send(`<h1>Something went wrong ${error}</h1>`);
       },
       onError(error) {
         console.error(error);
       },
     });
-
-    // const { pipe } = renderToPipeableStream(appHtml, {
-    //   onShellReady() {
-    //     templateBuild = templateBuild
-    //       .replace(`<div id="root"></div>`, `<div id="root">${appHtml}</div>`)
-    //       .replace('<!-- style -->', `<style>${style}"</style>`)
-    //       .replace(
-    //         '<!--preload-->',
-    //         `<script>window.__PRELOADED_STATE__ = ${JSON.stringify(preFetch).replace(
-    //           /</g,
-    //           '\\u003c'
-    //         )}</script>`
-    //       );
-
-    //     res.status(200).set({ 'Content-Type': 'text/html' }).end(templateBuild);
-    //     // pipe(res);
-    //   },
-    // });
-
-    // res.status(200).set({ 'Content-Type': 'text/html' }).end(templateBuild);
   });
 
   app.listen(5173, () => {
